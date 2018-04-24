20a63d03546b5d8c5ca44b8f3883a06a3f61e840
Bojie Jiang <jiangbojie@gmail.com>
2018-04-23 23:57:50 -0700

    5-1

---
+++ 5/5-1.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+f = open('py2.txt', 'w')
+
+s = u'你好'
+f.write(s.encode('gbk'))
+f.close()
+
+f = open('py2.txt', 'r')
+t = f.read()
+print t
+print t.decode('gbk')
+
+# Python3 看视频
