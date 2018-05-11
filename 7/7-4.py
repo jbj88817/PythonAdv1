d1fcef3f105932ff995a07d9944c0e0f2ec8c2e4
Bojie Jiang <jiangbojie@gmail.com>
2018-05-10 23:37:58 -0700

    7-4

---
+++ 7/7-4.py
@@ -0,0 +1,22 @@
+class Circle(object):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+
+    def set_radius(self, value):
+        if not isinstance(value, (int, long, float)):
+            raise ValueError('Wrong type: ')
+        self.radius = float(value)
+
+    def get_area(self):
+        return self.radius ** 2 * 3.14
+
+    R = property(get_radius, set_radius)
+
+
+c = Circle(3.2)
+print c.R
+c.R = 5.9
+print c.R
