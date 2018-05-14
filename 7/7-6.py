662a1bf5512c5e6985c40de8304c384b8109aa8e
Bojie Jiang <jiangbojie@gmail.com>
2018-05-13 20:26:45 -0700

    7-6

---
+++ 7/7-6.py
@@ -0,0 +1,27 @@
+class Attr(object):
+    def __init__(self, name, type_):
+        self.name = name
+        self.type_ = type_
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.type_):
+            raise TypeError('expected a %s' % self.type_)
+        instance.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.name]
+
+
+class Person(object):
+    name = Attr('name', str)
+    age = Attr('age', int)
+    height = Attr('height', float)
+
+
+p = Person()
+p.name = 'Bob'
+print p.name
+p.age = '17'  # would raise a exception
