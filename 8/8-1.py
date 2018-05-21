d503cd13f022ba26b2ce2143086795b8d04bd397
Bojie Jiang <jiangbojie@gmail.com>
2018-05-20 22:28:04 -0700

    8-1

--- 8/8-1.py
+++ 8/8-1.py
@@ -1,5 +1,13 @@
 # coding=utf-8
 # 如何使用多线程
 
-import threading
+from threading import Thread
 
+
+class MyThread(Thread):
+    def __init__(self, sid):
+        Thread.__init__(self)
+        self.sid = sid
+
+    def run(self):
+        pass
