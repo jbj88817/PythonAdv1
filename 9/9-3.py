1324438e31e01c74f50c44441c7dba6580038360
Bojie Jiang <jiangbojie@gmail.com>
2018-06-02 23:59:07 -0700

    9-3

--- 9/9-3.py
+++ 9/9-3.py
@@ -1,3 +1,11 @@
 # coding=utf-8
 # 定义带参数的装饰器
 # 提取函数签名 inspect.signature()
+
+
+def type_assert(*ty_args, **ty_kargs):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
